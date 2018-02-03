7032550d6e280be64a0c17ead1b71e849f7042ce
Peter Norman <october8th@hotmail.com>
2018-02-03 12:05:15 -0500

    we can add reservations

--- reservations.js
+++ reservations.js
@@ -97,6 +97,33 @@ app.post("/api/new", function(req, res) {
   res.json(newcharacter);
 });*/
 
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newReservation = req.body;
+  //console.log(newReservation);
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  var myReservation = {};
+
+  myReservation.name = newReservation.customerName;
+  myReservation.phone = newReservation.phoneNumber;
+  myReservation.email = newReservation.customerEmail;
+  myReservation.id = newReservation.customerID;
+
+  console.log(myReservation);
+
+  if(reservation.length < 5)
+  {
+  	reservation.push(myReservation);
+  }
+  else
+  {
+  	wait.push(myReservation);
+  }
+});
+
+
 // Starts the server to begin listening
 // =============================================================*/
 app.listen(PORT, function() {
